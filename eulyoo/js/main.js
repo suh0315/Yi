e5888d649c5cc802a3d1ee2b391a08055d19f748
suh0315 <suhyung0315@gmail.com>
2025-09-25 15:00:00 +0900

    .

--- eulyoo/js/main.js
+++ eulyoo/js/main.js
@@ -56,4 +56,22 @@ $(document).ready(function(){
     $(window).scroll(function(){
         scroll_chk()
     })
+
+    //book swiper
+    const book_swiper = new Swiper('.book .swiper', { /* 팝업을 감싼는 요소의 class명 */
+        slidesPerView: 2, /* 한번에 보일 팝업의 수 - 모바일 제일 작은 사이즈일때 */
+        spaceBetween: 16, /* 팝업과 팝업 사이 여백 */
+        breakpoints: {
+            769: {    /* 769px 이상일때 적용 */
+                slidesPerView: 'auto',    /*    'auto'   라고 쓰면 css에서 적용한 넓이값이 적용됨 */
+                spaceBetween: 24,
+            },
+        },
+        //centeredSlides: true, /* 팝업을 화면에 가운데 정렬(가운데 1번이 옴) */
+        navigation: {
+            nextEl: '.book .btn_next',
+            prevEl: '.book .btn_prev',
+        },
+    });
+
 })//document
