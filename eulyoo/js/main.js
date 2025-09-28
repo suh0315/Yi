bad111a765e0a4024fedfde4ee8ebf7365318c44
suh0315 <suhyung0315@gmail.com>
2025-09-28 20:06:00 +0900

    .

--- eulyoo/js/main.js
+++ eulyoo/js/main.js
@@ -73,5 +73,26 @@ $(document).ready(function(){
             prevEl: '.book .btn_prev',
         },
     });
+    const story_swiper = new Swiper('.story .swiper', { /* 팝업을 감싼는 요소의 class명 */
+	slidesPerView: 2, /* 한번에 보일 팝업의 수 - 모바일 제일 작은 사이즈일때 */
+	spaceBetween: 16, /* 팝업과 팝업 사이 여백 */
+	breakpoints: {
+		769: {    /* 640px 이상일때 적용 */
+			slidesPerView: 4,    /*    'auto'   라고 쓰면 css에서 적용한 넓이값이 적용됨 */
+			spaceBetween: 24,
+		},
+	},
+	//centeredSlides: true, /* 팝업을 화면에 가운데 정렬(가운데 1번이 옴) */
+	navigation: {
+		nextEl: '.story .btn_next',
+		prevEl: '.story .btn_prev',
+	}
+});
+    $('footer .top').on('click', function(){
+        // console.log('top')
+        $('html, body').animate({
+            scrollTop: 0
+        }, 500)
+    })
 
 })//document
