09fc5a0337532ca339d1a34f41630dbf562f7e2c
suh0315 <suhyung0315@gmail.com>
2025-10-15 17:32:33 +0900

    121
    111

--- dearpet/js/main.js
+++ dearpet/js/main.js
@@ -1,14 +1,15 @@
 $(document).ready(function(){
 
     //visual swiper :: 시작
+    let visual_time = 3000
     const visual_swiper = new Swiper('.visual .swiper', { /* 팝업을 감싼는 요소의 class명 */
 
-        // autoplay: {  /* 팝업 자동 실행 */
-        //     delay: 5500,
-        //     disableOnInteraction: true,
-        // },
+         autoplay: {  /* 팝업 자동 실행 */
+             delay: visual_time,
+             disableOnInteraction: true,
+         },
 
-        //effect: "fade", /* fade 효과 */
+        effect: "fade", /* fade 효과 */
 
         loop: true,  /* 마지막 팝업에서 첫번째 팝업으로 자연스럽게 넘기기 */
 
@@ -38,6 +39,14 @@ $(document).ready(function(){
                     $('.visual .ctrl_btn .paging .total').text(totalSlides);
                     // 현재 슬라이드 번호 표시
                     $('.visual .ctrl_btn .paging .current').text(this.realIndex + 1);
+                    $('.visual .ctrl_btn .paging .bar span').stop() //animate 종료
+                    $('.visual .ctrl_btn .paging .bar span').width(0)
+                    $('.visual .ctrl_btn .paging .bar span').animate({
+                        width: '100%'
+                    }, visual_time)
+                
+                    
+                    
                 },
                 slideChange: function () {
                     // 슬라이드 변경될 때 현재 번호 업데이트
@@ -45,7 +54,18 @@ $(document).ready(function(){
                 }
             }
     });
-    visual_swiper.autoplay.stop();  /* 일시정지 기능 */
-    visual_swiper.autoplay.start();  /* 재생 기능 */
+    $('.visual .ctrl_btn .stop').on('click', function(){
+        visual_swiper.autoplay.stop();  /* 일시정지 기능 */
+        $(this).hide()
+        $('visual .ctrl_btn .play').css('display', 'flex')
+        $('.visual .ctrl_btn .paging .bar span').stop() //animate 종료
+    })
+    $('.visual .ctrl_btn .play').on('click', function(){
+        visual_swiper.autoplay.start();  /* 재생 기능 */
+        $(this).hide()
+        $('visual .ctrl_btn .stop').css('display', 'flex')
+        updateCurrent();
+    })
+        
     //visual swiper :: 끝
 })//end
