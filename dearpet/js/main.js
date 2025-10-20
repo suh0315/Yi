264e2a68f468c1b64c05285f6b4f56839fb36d69
suh0315 <suhyung0315@gmail.com>
2025-10-20 23:13:19 +0900

    bb

--- dearpet/js/main.js
+++ dearpet/js/main.js
@@ -240,5 +240,24 @@ $(document).ready(function(){
 
     //끝 :: 찾습니다 tab
 
+    // 시작 :: 입양 swiper
+    const adopt_swiper = new Swiper('.adopt .swiper', { /* 팝업을 감싼는 요소의 class명 */
+        slidesPerView: 'auto', /* 한번에 보일 팝업의 수 - 모바일 제일 작은 사이즈일때 */
+        spaceBetween: 16, /* 팝업과 팝업 사이 여백 */
+        breakpoints: {
+            769: {    /* 769px 이상일때 적용 */
+                slidesPerView: 'auto',    /*    'auto'   라고 쓰면 css에서 적용한 넓이값이 적용됨 */
+                spaceBetween: 24,
+            },
+        },
+        centeredSlides: true, /* 팝업을 화면에 가운데 정렬(가운데 1번이 옴) */
+        loop: true,  /* 마지막 팝업에서 첫번째 팝업으로 자연스럽게 넘기기 */
+        navigation: {
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev',
+        },
+    });
+    
+    // 끝 :: 입양 swiper
 
 })//맨끝
