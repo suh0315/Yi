015319f5a7b6df4333eb245fa49cc11715bac5be
suh0315 <suhyung0315@gmail.com>
2025-10-21 12:05:58 +0900

    1313
    1

--- dearpet/js/main.js
+++ dearpet/js/main.js
@@ -253,11 +253,31 @@ $(document).ready(function(){
         centeredSlides: true, /* 팝업을 화면에 가운데 정렬(가운데 1번이 옴) */
         loop: true,  /* 마지막 팝업에서 첫번째 팝업으로 자연스럽게 넘기기 */
         navigation: {
-            nextEl: '.swiper-button-next',
-            prevEl: '.swiper-button-prev',
+            nextEl: '.adopt .ctrl_btn .next',
+            prevEl: '.adopt .ctrl_btn .prev',
         },
     });
     
     // 끝 :: 입양 swiper
+
+    // 시작 :: 후기 swiper
+    const review_swiper = new Swiper('.review .swiper', { /* 팝업을 감싼는 요소의 class명 */
+        slidesPerView: 'auto', /* 한번에 보일 팝업의 수 - 모바일 제일 작은 사이즈일때 */
+        spaceBetween: 16, /* 팝업과 팝업 사이 여백 */
+        breakpoints: {
+            769: {    /* 769px 이상일때 적용 */
+                slidesPerView: 4,    /*    'auto'   라고 쓰면 css에서 적용한 넓이값이 적용됨 */
+                spaceBetween: 24,
+            },
+        },
+        //centeredSlides: true, /* 팝업을 화면에 가운데 정렬(가운데 1번이 옴) */
+        loop: true,  /* 마지막 팝업에서 첫번째 팝업으로 자연스럽게 넘기기 */
+        navigation: {
+            nextEl: '.review .next',
+            prevEl: '.review .prev',
+        },
+    });
+
+    // 끝 :: 후기 swiper
 
 })//맨끝
