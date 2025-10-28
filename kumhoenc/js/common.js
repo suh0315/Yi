a40c7e56ff2cc8256b8878ccd68c171d297fafea
suh0315 <suhyung0315@gmail.com>
2025-10-28 15:52:24 +0900

    11

--- kumhoenc/js/common.js
+++ kumhoenc/js/common.js
@@ -86,4 +86,25 @@ $(document).ready(function(){
     $(window).scroll(function(){
         scroll_chk() //스크롤 할때마다 실행
     })
+
+    /* family_site 열고 닫기
+        footer .f_util .family_site .family_open 열기를 클릭하면
+        footer .f_util .family_site에 open 클래스 부여
+        footer .f_util .family_site .family_close 닫기를 클릭하면
+    */
+   $('footer .f_util .family_site .family_open').on('click', function(){
+        $('footer .f_util .family_site').addClass('open')
+        $('footer .f_util .family_site .family_wrap').slideDown()
+   })
+   $('footer .f_util .family_site .family_close').on('click', function(){
+        $('footer .f_util .family_site').removeClass('open')
+        $('footer .f_util .family_site .family_wrap').slideUp()
+    })
+
+    /* top버튼 클릭하면 상단으로 이동 */
+    $('footer .f_util .top').on('click', function(){
+        $('html, body').animate({
+            scrollTop : 0
+        }, 500)
+    })
 })//맨끝
