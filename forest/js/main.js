b36075054998739115b6160d75a0fbdc87299c19
suh0315 <suhyung0315@gmail.com>
2025-11-07 13:12:58 +0900

    111

--- forest/js/main.js
+++ forest/js/main.js
@@ -29,7 +29,7 @@ $(document).ready(function(){
 		},
 
 		responsiveWidth: 769, /* fullpage를 적용시키지 않을 모바일 사이즈 */
-        responsiveHeight: 500
+        responsiveHeight: 650
 	});//fullpage
 
 	let visual_name = ['2025 생명의숲 후원의달', '도시 속 나무심기', '후원자 인터뷰', '고목나무 이야기' ]
@@ -55,6 +55,23 @@ $(document).ready(function(){
 		},
 
 	});
+
+	const news_swiper = new Swiper('.news .swiper', { /* 팝업을 감싼는 요소의 class명 */
+		slidesPerView: 2, /* 한번에 보일 팝업의 수 - 모바일 제일 작은 사이즈일때 */
+		spaceBetween: 16, /* 팝업과 팝업 사이 여백 */
+		breakpoints: {
+			640: {    /* 640px 이상일때 적용 */
+				slidesPerView: 4,    /*    'auto'   라고 쓰면 css에서 적용한 넓이값이 적용됨 */
+				spaceBetween: 24,
+			},
+		},
+		//centeredSlides: true, /* 팝업을 화면에 가운데 정렬(가운데 1번이 옴) */
+		// loop: true,  /* 마지막 팝업에서 첫번째 팝업으로 자연스럽게 넘기기 */
+	});
+
+
+
+
 
 	/*
 		퀵메뉴 열고 닫기
