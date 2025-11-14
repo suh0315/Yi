2c224d87bf1d441f6cdf8eb8158e6dd45061d800
suh0315 <suhyung0315@gmail.com>
2025-11-14 13:14:48 +0900

    1111

--- busanMOCA/js/main.js
+++ busanMOCA/js/main.js
@@ -2,7 +2,7 @@ $(document).ready(function(){
     const visual_swiper = new Swiper('.visual .swiper', { /* 팝업을 감싼는 요소의 class명 */
 
 	autoplay: {  /* 팝업 자동 실행 */
-		delay: 5500,
+		delay: 5000,
 		disableOnInteraction: true,
 	},
 
@@ -11,15 +11,28 @@ $(document).ready(function(){
 	loop: true,  /* 마지막 팝업에서 첫번째 팝업으로 자연스럽게 넘기기 */
 
 	pagination: {  /* 몇개의 팝업이 있는지 보여주는 동그라미 */
-		el: '.swiper-pagination', /* 해당 요소의 class명 */
+		el: '.visual .paging ul', /* 해당 요소의 class명 */
 		clickable: true,  /* 클릭하면 해당 팝업으로 이동할 것인지 값 */
-		type: 'fraction',  /* type fraction을 주면 paging이 숫자로 표시됨 */
-		renderBullet: function (index, className) {   /* paging에 특정 코드 넣기 */
-		    return '<span class="' + className + '">' + (index + 1) + "</span>";
-		},
 	},
 
 });
+
+const showing_swiper = new Swiper('.showing .swiper', { /* 팝업을 감싼는 요소의 class명 */
+	slidesPerView: 2, /* 한번에 보일 팝업의 수 - 모바일 제일 작은 사이즈일때 */
+	spaceBetween: 16, /* 팝업과 팝업 사이 여백 */
+	breakpoints: {
+		1025: {    /* 1025px 이상일때 적용 */
+			slidesPerView: 'auto',    /*    'auto'   라고 쓰면 css에서 적용한 넓이값이 적용됨 */
+			spaceBetween: 24,
+		},
+		768: {    /* 640px 이상일때 적용 */
+			slidesPerView: 3,    /*    'auto'   라고 쓰면 css에서 적용한 넓이값이 적용됨 */
+			spaceBetween: 24,
+		},
+	},
+	//centeredSlides: true, /* 팝업을 화면에 가운데 정렬(가운데 1번이 옴) */
+});
+
 
 
 
