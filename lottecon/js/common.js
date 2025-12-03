bec7e535e1f06bee5c3a9931ac2ad5f2e602028f
suh0315 <suhyung0315@gmail.com>
2025-12-03 18:04:22 +0900

    1111

---
+++ lottecon/js/common.js
@@ -0,0 +1,31 @@
+$(document).ready(function(){
+
+    /* -----------------------------
+        1. 디바이스 체크
+    ----------------------------- */
+    let mobile_size = 1024;
+    let device_status = 'pc';
+
+    function device_chk(){
+        device_status = ($(window).width() > mobile_size) ? 'pc' : 'mobile';
+    }
+
+    device_chk();
+    $(window).on('resize', function () {
+
+        let prev_status = device_status; // 변경 전 상태 저장
+        device_chk(); // 변경 후 상태 갱신
+    
+        // PC → 모바일로 변경된 순간
+        if (prev_status === 'pc' && device_status === 'mobile') {
+            $('header').removeClass('menu_pc');
+            $('html').removeClass('no-scroll');
+        }
+    
+        // 모바일 → PC로 변경된 순간
+        if (prev_status === 'mobile' && device_status === 'pc') {
+            $('header').removeClass('menu_mo');
+            $('html').removeClass('no-scroll');
+        }
+    });
+})//맨끝
